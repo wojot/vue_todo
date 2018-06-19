3757c072220a355e91a28beb957c6e14e1483a48
wojot <wojotek@gmail.com>
2018-06-19 22:00:49 +0200

    tt

--- app.js
+++ app.js
@@ -1,3 +1,39 @@
+
+var config = {
+    apiKey: "AIzaSyBTtOwODYpew7fpl3MBRy6PiTydLW46ll8",
+    authDomain: "vuetodo-1af89.firebaseapp.com",
+    databaseURL: "https://vuetodo-1af89.firebaseio.com",
+    projectId: "vuetodo-1af89",
+    storageBucket: "vuetodo-1af89.appspot.com",
+    messagingSenderId: "1049113970688"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var task = new Vue({
+    el: '#task',
+    data: {
+        newTask:{
+            id: 0,
+            content: ''
+        }
+    },
+    // firebase: {
+    //     tasks: tasksRef
+    // },
+    methods: {
+        addTask: function() {
+            database.ref('tasks').push(this.newTask)
+            this.newTask.id = ''
+            this.newTask.content = ''
+        }
+    }
+})
+
+
+
+
 var app = new Vue({
     el: '#app',
     data: {
@@ -29,6 +65,6 @@ var app6 = new Vue({
     el: '#app-6',
     data: {
         message: 'Hello Vue!',
-        variable: 'test' 
+        variable: 'test'
     }
 })
---
+++ firebaseInit.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  // var config = {
+  //   apiKey: "AIzaSyBTtOwODYpew7fpl3MBRy6PiTydLW46ll8",
+  //   authDomain: "vuetodo-1af89.firebaseapp.com",
+  //   databaseURL: "https://vuetodo-1af89.firebaseio.com",
+  //   projectId: "vuetodo-1af89",
+  //   storageBucket: "vuetodo-1af89.appspot.com",
+  //   messagingSenderId: "1049113970688"
+  // };
+  // firebase.initializeApp(config);
