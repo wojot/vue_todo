3383f3087b54a9ecdcc42eba7502dc8949317886
Wojtek <wojotek@gmail.com>
2018-06-20 13:05:29 +0200

    changed some

--- app.js
+++ app.js
@@ -1,70 +1,46 @@
-
 var config = {
-    apiKey: "AIzaSyBTtOwODYpew7fpl3MBRy6PiTydLW46ll8",
-    authDomain: "vuetodo-1af89.firebaseapp.com",
-    databaseURL: "https://vuetodo-1af89.firebaseio.com",
-    projectId: "vuetodo-1af89",
-    storageBucket: "vuetodo-1af89.appspot.com",
-    messagingSenderId: "1049113970688"
+    apiKey: "AIzaSyAsoGaVPSfQPjaZRpb5ru6TQ_otmDf-k7c",
+    authDomain: "vue-todo-13c2b.firebaseapp.com",
+    databaseURL: "https://vue-todo-13c2b.firebaseio.com",
+    projectId: "vue-todo-13c2b",
+    storageBucket: "vue-todo-13c2b.appspot.com",
+    messagingSenderId: "177443944312"
   };
   firebase.initializeApp(config);
 
-var database = firebase.database();
-
-var task = new Vue({
-    el: '#task',
-    data: {
-        newTask:{
-            id: 0,
-            content: ''
-        }
-    },
-    // firebase: {
-    //     tasks: tasksRef
-    // },
-    methods: {
-        addTask: function() {
-            database.ref('tasks').push(this.newTask)
-            this.newTask.id = ''
-            this.newTask.content = ''
-        }
-    }
-})
-
-
-
+var tasksRef = firebase.database().ref('task');
 
 var app = new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
-    }
-})
-var app2 = new Vue({
-    el: '#app-2',
-    data: {
-        message: 'You loaded this page on ' + new Date().toLocaleString()
-    }
-})
-var app4 = new Vue({
-    el: '#app-4',
-    data: {
-        todos: [{
-                text: 'Learn JavaScript'
-            },
-            {
-                text: 'Learn Vue'
-            },
-            {
-                text: 'Build something awesome'
+        newTask: {
+            content: ''
+        }
+    },
+    firebase: {
+        task: tasksRef
+    },
+    methods: {
+        addTask: function() {
+            tasksRef.push(this.newTask)
+            this.newTask.content = ''
+        },
+        removeTask: function(task) {
+            tasksRef.child(task['.key']).remove()
+        },
+        edit: function(key) {
+            var inner = document.getElementById('edit'+key).innerHTML
+            if(inner == 'Save'){
+                document.getElementById('edit'+key).innerHTML = 'Edit'
+                document.getElementById('editInput'+key).disabled = true
+                var newContent = document.getElementById('editInput'+key).value
+                tasksRef.child(key).update({content: newContent})
+                tasksRef.child(key).remove()
+                M.toast({html: 'Changes saved!'})
+            } else if(inner == 'Edit') {
+                document.getElementById('edit'+key).innerHTML = 'Save'
+                document.getElementById('editInput'+key).disabled = false
             }
-        ]
-    }
-})
-var app6 = new Vue({
-    el: '#app-6',
-    data: {
-        message: 'Hello Vue!',
-        variable: 'test'
+        }
     }
 })
